06b9d26a3d5ff52545f81b40ef2263d4245c7cc8
Vineet <naikvin@gmail.com>
2011-12-11 23:07:11 +0530

    study Ackerman function and solve related 1.10

--- chapter01.scm
+++ chapter01.scm
@@ -98,3 +98,19 @@
 
 (define (improve guess x)
   (average guess (/ x guess)))
+
+;; 1.10
+;; Ackermann function 
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1) (A x (- y 1))))))
+
+(A 1 10) ;; 1024
+(A 2 4) ;; 65536
+(A 3 3) ;; 65536
+
+(define (f n) (A 0 n)) ;; 2n
+(define (g n) (A 1 n)) ;; (0|n=0) ; (2^n|n>0), 
+(define (h n) (A 2 n)) ;; (0|n=0) ; (2|n=1) ; (2^h(n-1))
