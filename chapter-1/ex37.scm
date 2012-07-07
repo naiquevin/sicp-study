f1b71a5ff804b43e5ce617bc536e0d9bbf4d7836
Vineet <naikvin@gmail.com>
2012-07-07 21:02:36 +0530

    Fix bug in 1.37 and add explanation too

--- chapter-1/ex37.scm
+++ chapter-1/ex37.scm
@@ -2,7 +2,7 @@
 (define (cont-frac nfunc dfunc k)
   (define (cont-frac-recur i)
     (if (= i k)
-        (/ (nfunc i) (dfunc i))
+        (/ (nfunc i) (dfunc i)) ; actually (/ (nfunc i) (+ (dfunc i) 0))
         (/ (nfunc i)
            (+ (dfunc i)
               (cont-frac-recur (+ i 1))))))
@@ -11,30 +11,37 @@
 
 (define (golden-inverse k func)  
   (func (lambda (i) 1.0)
-             (lambda (i) 1.0)
-             k))
+        (lambda (i) 1.0)
+        k))
 
 ;; phi val obtained from the google calculator
 (/ 1 1.61803399)
 ;Value: .6180339882723972
 
-(golden-inverse 10 cont-frac)
-;Value: .6179775280898876
-
 (golden-inverse 11 cont-frac)
 ;Value: .6180555555555556
 
-;; Value of k to get accuracy to 4 dec places = 11
+;; Value of k to get accuracy to 4 dec places = 10
+
+;; Iterative process
 
 (define (cont-frac-i nfunc dfunc k)
   (define (iter i result)
-    (if (= i 1)
+    (if (= i 0)
         result
         (iter (- i 1)
               (/ (nfunc i)
                  (+ (dfunc i)
                     result)))))
-  (iter k 1.0))
+  (iter k 0))
+
+
+;; result = 0 at the start of iteration because when we truncate
+;; the expansion at k, we are infact assuming that the value
+;; of cont-frac for k+1 = 0
+;; (Remember! for iterative process, start with known and iter from next)
+;; refer to the comment in recursive process above to understand 
+;; this better
 
 (golden-inverse 11 cont-frac-i)
 ;Value: .6180555555555556
