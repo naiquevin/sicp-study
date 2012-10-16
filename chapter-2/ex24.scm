606e9b586e81348ac7a71db759877cfffd1e2541
Vineet <naikvin@gmail.com>
2012-10-16 09:18:50 +0530

    Add scratch file with count-leaves implementation and solve ex2.24

---
+++ chapter-2/ex24.scm
@@ -0,0 +1,39 @@
+;; Exercise 2.24. Suppose we evaluate the expression (list 1 (list 2
+;; (list 3 4))). Give the result printed by the interpreter, the
+;; corresponding box-and-pointer structure, and the interpretation of
+;; this as a tree (as in figure 2.6).
+
+(load "scratch/trees.scm")
+
+(list 1 (list 2 (list 3 4)))
+
+;; Interpreter output
+;; (1 (2 (3 4)))
+
+
+;; Box-and-pointer structure
+
+;; [.][.]----[.][.]----[.][.]----[.][x]
+;;  |         |         |         |
+;;  |         |         |         |
+;;  1         2         3         4
+
+
+;; Interpretation as a tree
+
+;;     (1 (2 (3 4)))
+;;          .
+;;         / \
+;;        /   \
+;;       /     \
+;;      1      (2 (3 4))
+;;               .
+;;              / \
+;;             /   \
+;;            /     \
+;;           2     (3 4)
+;;                   .
+;;                  / \
+;;                 /   \
+;;                /     \
+;;               3       4
