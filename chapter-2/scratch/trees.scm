69a633710071e5fc5726e5c103c75dd17f3bc4e6
Vineet <naikvin@gmail.com>
2012-10-26 09:35:34 +0530

    Add the scale-tree functions from the 'Mapping over trees' section

--- chapter-2/scratch/trees.scm
+++ chapter-2/scratch/trees.scm
@@ -1,3 +1,4 @@
+;; Counting leaves
 
 (define (count-leaves x)
   (cond ((null? x) 0)
@@ -5,4 +6,29 @@
         (else 
          (+ (count-leaves (car x))
             (count-leaves (cdr x))))))
+
+;; Mapping over trees
+
+(define (scale-tree tree factor)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (* tree factor))
+        (else
+         (cons (scale-tree (car tree) factor)
+               (scale-tree (cdr tree) factor)))))
+
+(define t1 (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+
+(scale-tree t1 10)
+
+
+;; using map
+
+(define (scale-tree tree factor)
+  (map (lambda (subtree)
+         (if (pair? subtree)
+             (scale-tree subtree factor)
+             (* subtree factor)))
+       tree))
+
+(scale-tree t1 10)
 
