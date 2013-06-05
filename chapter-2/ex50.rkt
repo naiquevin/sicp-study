d2c5c56c195c639be09664125f03a160cac34346
Vineet <naikvin@gmail.com>
2013-06-05 09:08:54 +0530

    Fix rotate-270 in ex 2.50

--- chapter-2/ex50.rkt
+++ chapter-2/ex50.rkt
@@ -32,6 +32,6 @@
 
 (define (rotate-270 painter)
   ((transform-painter (make-vect 1.0 0.0)
-                      (make-vect 0.0 0.0)
-                      (make-vect 1.0 1.0))
+                      (make-vect 1.0 1.0)
+                      (make-vect 0.0 0.0))
    painter))
